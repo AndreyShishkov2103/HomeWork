867dc715b6df9647e1bbca92768b2a6eac19386e
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-01-19 18:21:50 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Меня зовут: Андрей Шишков");
+        System.out.println("Мне 43 года");
+        System.out.println("Я из Киева");
+    }
+}
