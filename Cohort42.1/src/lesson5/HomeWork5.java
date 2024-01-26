99b9d1508638c6f46807352a119efa7d43230a5b
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-01-26 16:52:12 +0200

    HW 5

--- Cohort42.1/src/lesson5/HomeWork5.java
+++ Cohort42.1/src/lesson5/HomeWork5.java
@@ -1,6 +1,7 @@
 package lesson5;
 
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * AIT-TR, Java Basic, Cohort42.1, HW #5
@@ -30,5 +31,34 @@ public class HomeWork5 {
         } else if (num3 == 3) {
             System.out.println("You entered: " + num3);
         } else System.out.println("Wrong number");
+
+        //task #3
+        Random random = new Random();
+        int num1N = random.nextInt(101);
+        int num2N = random.nextInt(101);
+        int num3N = random.nextInt(101);
+        int num4N = random.nextInt(101);
+        System.out.println("Generated numbers: " + num1N + ", " + num2N + ", " + num3N + ", " + num4N + ";");
+        int maxNum = Math.max(Math.max(num1N, num2N), Math.max(num3N, num4N));
+        System.out.println("The MAX number is: " + maxNum);
+
+        //task #4
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter 4 digit number: ");
+        String s = scanner1.nextLine();
+        if (s.length() != 4) {
+            System.out.println("Error: Please enter a four-digit number.");
+            System.exit(1);
+        }
+        int number1 = s.charAt(0) - '0';
+        int number2 = s.charAt(1) - '0';
+        int number3 = s.charAt(2) - '0';
+        int number4 = s.charAt(3) - '0';
+        boolean luckyT = (number1 + number2) == (number3 + number4);
+        if (luckyT) {
+            System.out.println("Congratulations! It's a lucky ticket.");
+        } else {
+            System.out.println("Sorry, not a lucky ticket.");
+        }
     }
 }
