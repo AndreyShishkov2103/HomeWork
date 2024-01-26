8d90d11f73515c3632498267fc91b18f858d4d7f
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-01-26 03:38:22 +0200

    HW 4

--- Cohort42.1/src/lesson5/Calculator.java
+++ Cohort42.1/src/lesson5/Calculator.java
@@ -8,19 +8,20 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter expression, Like 4 +/-/*/: 3:");
+        System.out.println("Enter expression, like 4 :/*/-/+ 3:");
         int a = scanner.nextInt();
         String s = scanner.next();
         int b = scanner.nextInt();
-        if (s.equals("+")) {
-            System.out.println(a + " s " + b + (a + b));
+        if (s.equals("+")){
+            System.out.println(a + " + " + b + " = " + (a + b));
         } else if (s.equals("-")) {
-            System.out.println(a + " s " + b + (a - b));
-        } else if (s.equals("*")) {
-            System.out.println(a + " s " + b + (a * b));
+            System.out.println(a + " - " + b + " = " + (a - b));
+        }else if (s.equals("*")) {
+            System.out.println(a + " * " + b + " = " + (a * b));
         } else if (s.equals(":")) {
-            System.out.println(a + " s " + b + (a / b));
-        } else
-        System.out.println("Error: unknown action" + s);
+            System.out.println(a + " : " + b + " = " + (a / b));
+        } else {
+            System.out.println("Error: unknown action: " + s);
+        }
     }
 }
