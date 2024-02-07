656085731a2d50878d03591333d566b9cb16eca1
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-02-07 19:40:54 +0200

    Lesson #10

---
+++ Cohort42.1/src/lesson10/Lesson10.java
@@ -0,0 +1,23 @@
+package lesson10;
+
+/**
+ * AIT-TR, Cohort 42.1, Java Basic, Lesson #10
+ * @version 2024-02-05
+ */
+public class Lesson10 {
+    public static void main(String[] args) {
+        // java compiler and JVM
+
+        // variables simple types == 8
+        int a = 25;
+        int b = 12 * 3 + a;
+
+        // logics
+        boolean f = true; // false
+        boolean l = f && true; // &&(and) ||(or) !(not)
+
+        // if-else
+        
+
+    }
+}
---
+++ Cohort42.1/src/lesson9/Lesson9.java
@@ -0,0 +1,42 @@
+package lesson9;
+
+/**
+ * AIT-TR, Cohort 42.1, Java Basic, Lesson #9
+ * @version 2024-02-02
+ */
+public class Lesson9 {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 2, 1};
+        printArray(arr);
+
+        int[] arr2 = {-5, -7, -1, -6, -5};
+        printArray(arr2);
+
+        int aa = 2;
+        int bb = 2;
+        int r1 = add(aa, bb);
+        int r2 = add(-4, 12);
+        System.out.println(r1 + ", " + r2);
+        System.out.println(add(3, 4, 5));
+    }
+
+    static int add(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    static int add (int... a) {
+        int s = 0;
+        for (int i : a) {
+            s += i;
+        }
+        return s;
+    }
+
+    static void printArray(int[] a) {
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
