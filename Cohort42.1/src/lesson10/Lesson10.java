2a28a376223419ccebcca6d5b8b8739d1b7cd844
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-02-09 13:47:03 +0200

    Update Lesson10.java

--- Cohort42.1/src/lesson10/Lesson10.java
+++ Cohort42.1/src/lesson10/Lesson10.java
@@ -6,7 +6,7 @@ package lesson10;
  */
 public class Lesson10 {
     public static void main(String[] args) {
-        // java compiler and JVM
+        // Java compiler and JVM
 
         // variables simple types == 8
         int a = 25;
@@ -14,10 +14,59 @@ public class Lesson10 {
 
         // logics
         boolean f = true; // false
-        boolean l = f && true; // &&(and) ||(or) !(not)
+        boolean l = f && true; // && (and) || (or) !(not)
 
-        // if-else
-        
+        // if - else
+        if (a == 3) {
+            System.out.println("a = 3");
+        } else {
+            System.out.println("a != 3");
+        }
+        String s = a == 3? "a = 3" : "a != 3";  // ternary operator
 
+        // switch
+        switch (a) {
+            case 1, 2, 3:
+                System.out.println("a == 1 or 2 or 3");
+                break;
+            // case N:
+            default:
+                System.out.println("Alternative reality");
+        }
+
+        // loops while, do-while
+        while (a< 10) {
+            // TODO write loop body
+            a++;
+        }
+
+        // loops for with 2 counters
+        for (int i = 0, j = 4; i < 5; i++, j--) {
+            System.out.println("i = " + i + " j = " + j);
+        }
+
+        // array and for
+        int[] arr = new int[10];
+        int[] arr1 = {4, 8, 1, 12, -3};
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        // using break && continue
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 2) {
+                //break;
+                continue;
+            }
+            System.out.println(arr[i] + " ");
+        }
+
+        // method(s)
+        int sum = add(5, 6);
+        System.out.println(sum);
+    }
+    static int add(int a, int b) {
+        return a + b;
     }
 }
