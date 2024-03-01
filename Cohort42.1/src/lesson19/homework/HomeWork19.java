de526e0697bec1665f0536805b6be05b2936d4a6
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-03-01 10:53:27 +0200

    HW #19

---
+++ Cohort42.1/src/lesson19/homework/HomeWork19.java
@@ -0,0 +1,23 @@
+package lesson19.homework;
+
+/**
+ * AIT-TR, Java Basic, Cohort42.1, HW #19
+ * @author Andrey Shishkov
+ * @version 2024-03-01
+ */
+public class HomeWork19 {
+    public static void main(String[] args) {
+        RubberArray rb = new RubberArray();
+        rb.add(8);
+        rb.add(4);
+        rb.add(3);
+        rb.add(2);
+        rb.add(1);
+        System.out.println(rb);
+        System.out.println(rb.contains(12));
+        System.out.println(rb.indexOf(12));
+        System.out.println(rb.indexOf(5));
+        rb.add(-3, 3);
+        System.out.println(rb);
+    }
+}
