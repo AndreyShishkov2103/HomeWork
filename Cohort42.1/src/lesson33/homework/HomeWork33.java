5f80df00f760f10829bf097f113f254de9f93354
AndreyShishkov2103 <andrii.shyshkov@gmail.com>
2024-04-05 19:48:07 +0300

    Create HomeWork33.java

---
+++ Cohort42.1/src/lesson33/homework/HomeWork33.java
@@ -0,0 +1,31 @@
+package lesson33.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * AIT-TR, Java Basic, Cohort42.1, HW #33
+ * @author Andrey Shishkov
+ * @version 2024-04-05
+ */
+public class HomeWork33 {
+    public static void main(String[] args) {
+        List<Integer> listOne = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        List<Integer> listTwo = new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7));
+        List<Integer> listThree = new ArrayList<>(Arrays.asList(6, 7, 8, 9, 0));
+
+        System.out.println(GetInterSectionOfLists(listOne, listTwo));
+        System.out.println(GetInterSectionOfLists(listTwo, listThree));
+    }
+
+    static <T> List<T> GetInterSectionOfLists(List<T> listOne, List<T> listTwo) {
+        List<T> result = new ArrayList<>();
+        for (T item : listOne) {
+            if (listTwo.contains(item)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+}
